996f40cac68ffba60264534d48499b0a7ef11b90
Vittoria Carpeggiani <vccarpeggiani@gmail.com>
2024-11-17 20:12:56 -0300

    jogo lindo

--- jogo.py
+++ jogo.py
@@ -34,6 +34,8 @@ HEIGHT = 720
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('jogo do pato')
 macaco_img = pygame.image.load('assets/img/macaco.png').convert_alpha()
+inicio_image = pygame.image.load('assets/img/telainicio.webp')
+inicio_image = pygame.transform.scale(inicio_image, (1080, 720)) 
 game_over_image = pygame.image.load('assets/img/game_over.webp')
 game_over_image = pygame.transform.scale(game_over_image, (1080, 720)) 
 assets = {}
@@ -44,9 +46,11 @@ assets['fonte2'] = pygame.font.Font(fonte2,28)
 assets['pontos'] = 0
 assets['tempo'] = 0
 assets['tempo_inicial'] = 0
-pygame.mixer.music.load('assets/snd/duckmusica.mp3')  # Música de fundo]
-pygame.mixer.music.play(loops=-1)  # Loop infinito
-pygame.mixer.music.set_volume(0.5)  # Ajuste o volume (0.0 a 1.0)
+#sons usados para matar e fundo(original)
+som_acerto = pygame.mixer.Sound('assets/snd/fortnite-gun-shot-sound.mp3')
+pygame.mixer.music.load('assets/snd/duckmusica.mp3') 
+pygame.mixer.music.play(loops=-1)  
+pygame.mixer.music.set_volume(0.5)  
 
 # ----- Inicia estruturas de dados
 # Definindo os novos tipos
@@ -64,12 +68,16 @@ class Pato(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         super().__init__()
-        self.spritesheet = pygame.image.load(path.join('assets\img\patoduckhunt.png')).convert_alpha()
-        self.spritesheet = pygame.transform.scale(self.spritesheet, (100,100))
-        self.sprites = load_spriteshhet(self.spritesheet,3,5)
+        self.name = "Patonormal"
+        self.spritesheet = pygame.image.load(path.join('assets\img\patoduckhunt_combined.png')).convert_alpha()
+        self.spritesheet = pygame.transform.scale(self.spritesheet, (200,100))
+        self.sprites = load_spriteshhet(self.spritesheet,1,6)
         self.animations= {
             'left':self.sprites[0:2],
-            'stay_left': self.sprites[0]
+            'stay_left': self.sprites[0],
+            'right':self.sprites[3:5],
+            
+
         }
         self.state='stay_left'
         self.animation = self.animations[self.state]
@@ -82,10 +90,14 @@ class Pato(pygame.sprite.Sprite):
         
         if randint(1, 2) == 1:
             # Lado direito
+            self.sprite_speed=5
+            self.state = 'right'
             self.rect.x = randint(1070, 1080)
             self.speedx = randint(-5, -3)
         else:
             # Lado esquerdo
+            self.sprite_speed=5
+            self.state = 'left'
             self.rect.x = randint(10, 60)
             self.speedx = randint(3, 5)
         
@@ -94,17 +106,20 @@ class Pato(pygame.sprite.Sprite):
 
 #atualiza as posicoes e tudo
     def update(self):
-        self.state='left'
+        
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 
         if self.rect.x <= 0 or self.rect.y <= 0 or self.rect.x > 1080 or self.rect.y > 720:
             if randint(1, 2) == 1:
             # Lado direito
+                self.state='right'
+                self.sprite_speed=5
                 self.rect.x = randint(1070, 1080)
                 self.speedx = randint(-5, -3)
             else:
             # Lado esquerdo
+                self.state='left'
                 self.sprite_speed=5
                 self.rect.x = randint(10,60)
                 self.speedx = randint(3, 5)
@@ -125,24 +140,47 @@ class Pato(pygame.sprite.Sprite):
         self.image=self.animations[self.state][self.frame]
         self.rect=self.image.get_rect()
         self.rect.center=center
+
 class PatoFAST(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("assets/img/patosfast.png")
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect()  # Corrige para usar self.rect
+        super().__init__()
+        self.name = "Patoroxo"
+        self.spritesheet = pygame.image.load(path.join('assets\img\patonovo.png')).convert_alpha()
+        self.spritesheet = pygame.transform.scale(self.spritesheet, (200,100))
+        self.sprites = load_spriteshhet(self.spritesheet,1,6)
+        self.animations= {
+            'left':self.sprites[0:2],
+            'stay_left': self.sprites[0],
+            'right':self.sprites[3:5],
+            
+
+        }
+        self.state='stay_left'
+        self.animation = self.animations[self.state]
+        self.frame=0
+        self.sprite_speed=0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_ticks = 300
+        self.image = self.animation if isinstance(self.animation,list) else self.animation
+        self.rect = self.image.get_rect()
         
         if randint(1, 2) == 1:
             # Lado direito
+            self.sprite_speed=5
+            self.state = 'right'
             self.rect.x = randint(1070, 1080)
-            self.speedx = randint(-7, -4)
+            self.speedx = randint(-5, -3)
         else:
             # Lado esquerdo
+            self.sprite_speed=5
+            self.state = 'left'
             self.rect.x = randint(10, 60)
-            self.speedx = randint(4, 7)
+            self.speedx = randint(3, 5)
         
         self.rect.y = randint(0, 720)
-        self.speedy = randint(-1, 1)
+        self.speedy = randint(-1, 1)  
+        
 
     def update(self):
         self.rect.x += self.speedx
@@ -151,15 +189,34 @@ class PatoFAST(pygame.sprite.Sprite):
         if self.rect.x <= 0 or self.rect.y <= 0 or self.rect.x > 1080 or self.rect.y > 720:
             if randint(1, 2) == 1:
                 # Lado direito
+                self.state='right'
+                self.sprite_speed=5
                 self.rect.x = randint(1070, 1080)
                 self.speedx = randint(-7, -4)
             else:
                 # Lado esquerdo
+                self.state='left'
+                self.sprite_speed=5
                 self.rect.x = randint(10, 60)
                 self.speedx = randint(4, 7)
             
             self.rect.y = randint(0, 720)
             self.speedy = randint(-1, 1)
+        if self.sprite_speed!=0:
+            self.animate()
+        
+    def animate(self):
+        now=pygame.time.get_ticks()
+        elapsed_ticks=now - self.last_update
+        if elapsed_ticks > self.frame_ticks:
+            self.last_update=now
+            self.frame+=1
+            if self.frame>=len(self.animations[self.state]):
+                self.frame=0
+        center= self.rect.center
+        self.image=self.animations[self.state][self.frame]
+        self.rect=self.image.get_rect()
+        self.rect.center=center
 
 
 
@@ -197,17 +254,20 @@ while game != False:
                 for Pato in all_sprites:  # Itera sobre todos os sprites
         # Verifica se o clique está dentro do retângulo do sprite
                     if (Pato.rect.x <= event.pos[0] <= Pato.rect.x + Pato.rect.width and Pato.rect.y <= event.pos[1] <= Pato.rect.y + Pato.rect.height):
+                        if Pato.name == "Patonormal":
             # Adiciona pontos (diferente para Pato e PatoFAST)
-                        assets['pontos'] += 1  # A pontuação é definida na classe
+                            som_acerto.play()
+                            assets['pontos'] += 1  # A pontuação é definida na classe
                         
-                        Pato.rect.x = -100
-                        Pato.rect.y = -100
+                            Pato.rect.x = -100
+                            Pato.rect.y = -100
 
-                for PatoFAST in all_sprites:
-                    if PatoFAST.rect.x <= event.pos[0] <= PatoFAST.rect.x+50 and PatoFAST.rect.y <= event.pos[1] <= PatoFAST.rect.y+50:
-                         assets['pontos'] += 3
-                         PatoFAST.rect.x =-100
-                         PatoFAST.rect.y =-100
+    
+                        elif Pato.name == "Patoroxo":
+                            som_acerto.play()
+                            assets['pontos'] += 3
+                            Pato.rect.x =-100
+                            Pato.rect.y =-100
                         
         # ----- Gera saídas
         texto_pontos = assets['fonte'].render(f"{assets['pontos']}", True, (255, 255, 0))
@@ -233,8 +293,8 @@ while game != False:
         assets['pontos'] = 0
         assets['tempo_inicial'] += (clock.get_time()/1000)
         font = pygame.font.SysFont(None, 48)
-        texto_tela_inicial = assets['fonte'].render('MATA PATO', True, (0, 0, 255))
         texto_tela_inicial2 = assets['fonte'].render('Precione espaço para começar', True, (0, 0, 255))
+       
         for event in pygame.event.get():
             # ----- Verifica consequências
             if event.type == pygame.QUIT:
@@ -242,9 +302,13 @@ while game != False:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     game ='jogo'
-        window.fill((0, 0, 0))  # Preenche com a cor branca
-        window.blit(texto_tela_inicial,(WIDTH/2-40,HEIGHT/2-10))
-        window.blit(texto_tela_inicial2,(WIDTH/2-40,240))
+         
+        window.blit(inicio_image, ((WIDTH - inicio_image.get_width()) // 2, (HEIGHT - inicio_image.get_height()) // 2))
+        
+        texto_tela_inicial2_rect = texto_tela_inicial2.get_rect()
+        texto_tela_inicial2_rect.center = (WIDTH // 2+10, HEIGHT // 2 + 200)
+        pygame.draw.rect(window, (250, 250, 250), texto_tela_inicial2_rect)
+        window.blit(texto_tela_inicial2,(WIDTH/2-200,550))
         pygame.display.update() 
          # Mostra o novo frame para o jogador
     while game == 'gameover':
@@ -266,7 +330,7 @@ while game != False:
         window.blit(game_over_image, ((WIDTH - game_over_image.get_width()) // 2, (HEIGHT - game_over_image.get_height()) // 2))  # Centraliza a imagem
         texto_pontuacao_final = assets['fonte'].render(f"SCORE: {assets['pontos']}", True, (255, 255, 255))
         texto_pontuacao_final_rect = texto_pontuacao_final.get_rect()
-        texto_pontuacao_final_rect.center = (WIDTH // 2, HEIGHT // 2 + 250)  # Ajuste a posição conforme necessário
+        texto_pontuacao_final_rect.center = (WIDTH // 2, HEIGHT // 2 + 250)  
         pygame.draw.rect(window, (0, 0, 0), texto_pontuacao_final_rect) # Desenha o retângulo preto
 
     # Renderiza o texto da pontuação final por cima do retângulo
